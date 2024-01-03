eca74eab4011260ccb01881690889a51ab4c19ac
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-03 22:50:50 +0530

    delete records from ui

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -1,44 +1,70 @@
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
+// RECORD COMPONENT
+function Record(props) {
+  return (
+    <>
+      <tr>
+        <td>{props.record.name}</td>
+        <td>{props.record.address}</td>
+        <td>
+          <Link className="btn btn-link">Edit</Link>
+          <button className="btn btn-link" onClick={()=>{
+            props.deleteRecord(props.record._id)
+          }}>Delete</button>
+        </td>
+      </tr>
+    </>
+  );
+}
 
 function RecordList() {
   const [records, setRecords] = useState([]);
-
   async function getRecords() {
     try {
       const response = await fetch("http://localhost:3000/record");
+      if (!response.ok) {
+        const message = `An error occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
       const data = await response.json();
       setRecords(data);
     } catch (error) {
-      console.error("Error fetching records:", error);
+      console.log(error);
     }
   }
-
   useEffect(() => {
     getRecords();
   }, []);
 
+  async function deleteRecord(id){
+    await fetch(`http://localhost:3000/record/${id}`,{
+      method:"DELETE"
+    })
+    const newRecords=records.filter((el)=>el._id!==id);
+    setRecords(newRecords);
+  }
+  const recordList = records.map(record => (
+    <Record
+      record={record}
+      key={record._id}
+      deleteRecord={()=>deleteRecord(record._id)}
+    />
+  ));
+  
   return (
     <>
       <div>RecordList</div>
       <table className="table table-striped" style={{ marginTop: "20px" }}>
-        <thead className="thead-dark">
+        <thead>
           <tr>
-            <th>SL No.</th>
             <th>Name</th>
             <th>From</th>
             <th>Action</th>
-          </tr>
+          </tr>  
         </thead>
-        <tbody>
-          {records.map((record, index) => (
-            <tr key={index}>
-              <th scope="row">{index + 1}</th>
-              <td>{record.name}</td>
-              <td>{record.address}</td>
-              {/* <td>{record.}</td> */}
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{recordList}</tbody>
       </table>
     </>
   );
